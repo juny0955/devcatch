68e07abff1a9c84e94f16bb4512abcaef0730537
h0ch1 <h0ch1@naver.com>
2025-03-01 23:38:31 +0900

    feature: 네이버 테크 블로그 추가 네이버는 rss feed에 publishedDate 제공 하지않음 따라서 updatedDate 사용으로 대응

--- src/main/java/com/davcatch/devcatch/service/schedue/article/ArticleSchedulerService.java
+++ src/main/java/com/davcatch/devcatch/service/schedue/article/ArticleSchedulerService.java
@@ -1,6 +1,9 @@
 package com.davcatch.devcatch.service.schedue.article;
 
+import java.time.LocalDate;
+import java.time.ZoneId;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -23,18 +26,33 @@ public abstract class ArticleSchedulerService {
 	 */
 	abstract public void createNewArticles() throws CustomException;
 
-	/**
-	 * RSS 피드의 각 엔트리를 Article로 변환하고, 마지막 게시글 이후의 것들만 반환
+	/** Article 필터
+	 * 해당 Source에 해당하는 데이터가 없을 시 어제 날짜 기준 이후 데이터 변환
+	 * 해당 Source에 해당하는 데이터가 있을 시 마지막 게시글 이후의 것들만 변환
+	 *
+	 * @param feed 필터링할 feed
+	 * @param source 해당 Source
+	 * @param lastPublishedArticle 해당 Source의 마지막 등록된 Article
 	 */
 	protected List<Article> filterAndCreateArticles(SyndFeed feed, Source source, Optional<Article> lastPublishedArticle) {
-
 		List<Article> articles = new ArrayList<>();
+		Date date;
+
+		if (lastPublishedArticle.isEmpty()) {
+			LocalDate yesterDay = LocalDate.now().minusDays(1);
+			date = Date.from(yesterDay.atStartOfDay(ZoneId.systemDefault()).toInstant());
+		} else
+			date = lastPublishedArticle.get().getPublishedAt();
+
+
 		for (SyndEntry entry : feed.getEntries()) {
-			// 이전 데이터가 없거나, 마지막 게시글 이후의 게시글만 처리
-			if (lastPublishedArticle.isEmpty() || entry.getPublishedDate().after(lastPublishedArticle.get().getPublishedAt())) {
+			// RSS Feed에 PublishedDate가 없는 경우 UpdateDate로 사용
+			Date entryDate = entry.getPublishedDate() != null ? entry.getPublishedDate() : entry.getUpdatedDate();
+
+			if (entryDate != null && entryDate.after(date))
 				articles.add(articleFactory.createArticle(source, entry));
-			}
 		}
+
 		return articles;
 	}
 }
